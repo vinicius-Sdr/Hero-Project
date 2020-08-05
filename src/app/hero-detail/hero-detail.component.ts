1001bbea33e20654040dd21e1189461a3dcf784d
Vinicius Santana dos Reis <vini.santana.612@gmail.com>
2020-08-05 13:39:28 -0300

    parte 3 finalizada

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Hero } from '../hero';
+import { HeroesComponent } from '../heroes/heroes.component';
+
 
 @Component({
   selector: 'app-hero-detail',
@@ -6,6 +9,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
+  @Input() hero: Hero;
+
 
   constructor() { }
 
